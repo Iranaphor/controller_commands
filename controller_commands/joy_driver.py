615ad5f6cdd4146aa7560dcb3e460e2897e33eda
James R Heselden <primordia@live.com>
2024-07-15 21:07:19 +0100

    params updates

--- controller_commands/joy_driver.py
+++ controller_commands/joy_driver.py
@@ -31,18 +31,25 @@ class JoyDriver(Node):
         self.get_logger().info('🎮 | ROS params declared\n')
 
         # Check for joy trigger
-        self.reset_button = 5
-        self.spawn_button = 8
         self.prior = Joy()
         self.prior.buttons = [0]*16
         self.joy_sub = self.create_subscription(Joy, '/joy', self.joy_cb, 10)
 
-        # Load config
-        PKG = get_package_share_directory('controller_commands')
-        params_file = f'{PKG}/config/conf.yaml'
-        with open(params_file) as f:
-            self.actions = yaml.load(f)
-        print(self.actions)
+        # Load params config
+        if not self.has_parameter('action_modifier'):
+            self.declare_parameter('action_modifier', int(4))
+        self.modifier_control = self.get_parameter('action_modifier').value
+        self.modifier = ''
+
+        for k in ['', '_alt']:
+            for j in ['press', 'release']:
+                for i in range(0,15):
+                    if not self.has_parameter(f'{j}{k}.{i}'):
+                        self.declare_parameter(f'{j}{k}.{i}', str(''))
+        #self.declare_parameter('press_alt')
+        #self.declare_parameter('release')
+        #self.declare_parameter('release_alt')
+        #self.declare_parameter('release_alt')
 
 
     ##############################
@@ -51,14 +58,21 @@ class JoyDriver(Node):
 
         for b in range(0,len(msg.buttons)):
 
+            # Switch to L1 alternative bindings
+            if msg.buttons[self.modifier_control] and not self.prior.buttons[self.modifier_control]:
+                self.modifier = '_alt'
+            if not msg.buttons[self.modifier_control] and self.prior.buttons[self.modifier_control]:
+                self.modifier = ''
+
+            # Execute associated command
             if msg.buttons[b] and not self.prior.buttons[b]:
-                cmd = self.actions[b]['press']
-                self.get_logger().info(f'🎮  | button {b} pressed:  performing cmd: {cmd}')
+                cmd = self.get_parameter(f'press{self.modifier}.{b}').value
+                self.get_logger().info(f'🎮  | button {b}{self.modifier} pressed:  performing cmd: {cmd}')
                 subprocess.run(cmd, shell=True)
 
             if not msg.buttons[b] and self.prior.buttons[b]:
-                cmd = self.actions[b]['release']
-                self.get_logger().info(f'🎮  | button {b} released: performing cmd: {cmd}')
+                cmd = self.get_parameter(f'release{self.modifier}.{b}').value
+                self.get_logger().info(f'🎮  | button {b}{self.modifier} released: performing cmd: {cmd}')
                 subprocess.run(cmd, shell=True)
 
         self.prior = msg
--- launch/driver.launch.py
+++ launch/driver.launch.py
@@ -5,20 +5,20 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     # Locate parameter file
-#    PKG = get_package_share_directory('controller_commands')
-#    params_file = f'{PKG}/config/params.yaml'
+    PKG = get_package_share_directory('controller_commands')
+    params_file = f'{PKG}/config/params.yaml'
 
     # Construct Launch Description
     LD = LaunchDescription()
 
-    # Spawner (aka NestBuilder)
+    # Joy Driver
     LD.add_action(Node(package='controller_commands',
                        executable='joy_driver.py',
                        name='joy_driver',
-#                       parameters=[params_file]
+                       parameters=[params_file]
                        ))
 
-    # Communication (aka Quacker)
+    # Joy Node
     LD.add_action(Node(package='joy',
                        executable='joy_node',
                        name='joy_node'))
